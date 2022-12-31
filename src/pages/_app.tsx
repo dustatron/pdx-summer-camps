7e32f045f918f2b8b3cf12a6386c365754fb20cf
Dusty McCord <dusty@Dustys-MacBook-Pro.local>
2022-12-30 17:54:54 -0800

    remove unused import

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,12 +4,8 @@ import { SessionProvider } from "next-auth/react";
 import { MapProvider } from "react-map-gl";
 import { ChakraProvider } from "@chakra-ui/react";
 import AlertContextProvider from "../context/AlertContext";
-
 import "mapbox-gl/dist/mapbox-gl.css";
-import "react-map-gl-geocoder/dist/mapbox-gl-geocoder.css";
-
 import { trpc } from "../utils/trpc";
-
 import "../styles/globals.css";
 
 import PageLayout from "../components/Styled/PageLayout";
